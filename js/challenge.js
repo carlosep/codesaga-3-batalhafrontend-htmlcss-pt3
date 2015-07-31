2a74629d257e2d581b1853087dbd709c774ccf09
Joao Almeida <joaorsalmeida@gmail.com>
2015-07-31 00:52:01 -0300

    Add util.js and reuse js functions

--- js/challenge.js
+++ js/challenge.js
@@ -1,31 +1,13 @@
 $(document).ready(function() {
   $('#solution').load(function() {
-    var h1 = $("#solution").contents().find("h1");
-    var img = $("#solution").contents().find("img");
+    var h1 = getElement("h1");
+    var img = getElement("img");
     var errors = new Array();
-    
-    if (h1.css("color") != "rgb(255, 0, 0)"){
-      errors.push("Your \'h1\' tag is not \'red\'.")
-    }
-    
-    if (img.attr("src") == null){
-      errors.push("Your image should be valid.")
-    }
-    
-    var result = $("#result");
-    if (errors.length == 0){
-      result.addClass("alert-success");
-      result.append("Success. You are ready to the next challenge.")
-    }
-    else {
-      result.addClass("alert-danger");
-      result.append("Ops... You still have " + errors.length + " errors:<br>");
-      result.append(errors.join("<br>"));
-    }
+
+    verify(h1.css("color") != "rgb(255, 0, 0)", "Your \'h1\' tag is not \'red\'.", errors);
+    verify(img.attr("src") == null, "Your should add an image using the tag \'img\'.", errors);
+
+    showMessage(errors);
+
   });
 });
-
-function strcmp(a, b)
-{   
-  return (a<b?-1:(a>b?1:0));  
-}
